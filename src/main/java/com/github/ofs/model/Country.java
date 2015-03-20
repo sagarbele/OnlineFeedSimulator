8128e086bdc85fb060571952d7bfb723883db734
Sagar Bele <sagarb.4488@gmail.com>
2015-03-20 12:32:07 +0530

    updated column names

--- src/main/java/com/github/ofs/model/Country.java
+++ src/main/java/com/github/ofs/model/Country.java
@@ -14,29 +14,26 @@ public class Country {
 
 	@Id
 	@GeneratedValue
-	private Long id;
+	private Long countryId;
 	
 	@NotEmpty
 	@Size(min=3, max=20)
-	private String name;
+	private String countryName;
 
-	public Long getId() {
-		return id;
+	public Long getCountryId() {
+		return countryId;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setCountryId(Long countryId) {
+		this.countryId = countryId;
 	}
 
-	public String getName() {
-		return name;
+	public String getCountryName() {
+		return countryName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
 	}
-	
-	
-	
 	
 }
